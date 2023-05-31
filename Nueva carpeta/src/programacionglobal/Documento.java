08746e4653b474e871a049089518a2c78102dbb7
roAscurra <rominaascurra4887@gmail.com>
2023-05-31 15:31:28 -0300

    Clases del diagrama uml

---
+++ Nueva carpeta/src/programacionglobal/Documento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package programacionglobal;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Documento {
+    
+}
